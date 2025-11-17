a735df3f42e1c7cc50a0b7782d73518d83b99f40
ZEPHYR1702 <keffkaunderage@gmail.com>
2025-11-17 13:14:55 +0700

    week10 added

---
+++ all latihan/week10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace all_latihan
+{
+    internal class week10
+    {
+        public static void pilihan()
+        {
+            Console.WriteLine("Latihan Week 10");
+            // Tambahkan pilihan latihan untuk week 10 di sini
+        }
+    }
+}
